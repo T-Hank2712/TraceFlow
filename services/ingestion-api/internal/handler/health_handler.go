a073198ffad389e6ffedd1fe5feca0db13d5a34b
LtThanh <lamtanthanh2005@gmail.com>
2026-08-28 11:56:40 +0700

    feat(ingestion): add Kafka health check

--- services/ingestion-api/internal/handler/health_handler.go
+++ services/ingestion-api/internal/handler/health_handler.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/T-Hank2712/traceflow/ingestion-api/internal/producer"
 )
 
 func HealthHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,4 +13,26 @@ func HealthHandler(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewEncoder(w).Encode(map[string]string{
 		"status": "OK",
 	})
+}
+
+func KafkaHealthHandler(producer *producer.KafkaProducer) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		if err := producer.HealthCheck(); err != nil {
+			w.WriteHeader(http.StatusServiceUnavailable)
+
+			_ = json.NewEncoder(w).Encode(map[string]string{
+				"status": "UNAVAILABLE",
+			})
+
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+
+		_ = json.NewEncoder(w).Encode(map[string]string{
+			"status": "Kafka OK",
+		})
+	}
 }
